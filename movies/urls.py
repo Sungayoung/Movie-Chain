0d0070a86191b05b6e8e7483884e2c6d318d73da
okdongdong <kjch6411@naver.com>
2021-11-17 17:34:12 +0900

    [ADD] movies url 구성

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from . import views
 
 app_name='movies'
 
 urlpatterns = [
-    
+    path('', views.get_movie_list),
+    path('<int:movie_pk>/', views.get_movie_detail),
+    path('<int:movie_pk>/actors/', views.get_actor_list),
+    path('<int:movie_pk>/crews/', views.get_crew_list),
+    path('<int:movie_pk>/reviews/', views.get_or_create_review),
+    path('reviews/<int:review_pk>/', views.update_or_delete_review_or_get_or_create_comment_list),
+    path('comments/<int:comment_pk>/', views.update_or_delete_comment),
 ]
