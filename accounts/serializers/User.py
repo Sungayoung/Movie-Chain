a065359589add3bf456e32db753794f7a9da2f49
okdongdong <kjch6411@naver.com>
2021-11-17 17:41:55 +0900

    [ADD] accounts url 구성

--- accounts/serializers/User.py
+++ accounts/serializers/User.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from ..models import User
 from ...movies.models import Genre, Movie
+from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     profile_img = serializers.ImageField(use_url=True)
+
     class MovieListSerializer(serializers.ModelSerializer):
         class Meta:
             model = Movie
@@ -15,13 +17,22 @@ class UserSerializer(serializers.ModelSerializer):
         class Meta:
             model = Genre
             fields = '__all__'
-    
 
     like_genres = GenreSerializer(many=True, read_only=True)
     personal_movies = MovieListSerializer(many=True, read_only=True)
     bookmark_movies = MovieListSerializer(many=True, read_only=True)
     favorite_movies = MovieListSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = User
-        fields = ('pk', 'email', 'nickname', 'like_genres', 'personal_movies', 'bookmark_movies', 'favorite_movies')
+        fields = ('pk', 'email', 'nickname', 'like_genres', 'personal_movies',
+                  'bookmark_movies', 'favorite_movies', 'profile_img')
+
+
+class SignupSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'nickname', 'password')
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 app_name='accounts'
 
 urlpatterns = [
-    
+    path('signup/', views.signup),
+    path('api-token-auth/', obtain_jwt_token)
 ]
