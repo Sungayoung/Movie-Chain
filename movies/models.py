9f999471464ca0da389a008a4044478c56f01f31
okdongdong <kjch6411@naver.com>
2021-11-17 19:28:53 +0900

    [DEBUG] 에러디버깅

--- movies/models.py
+++ movies/models.py
@@ -1,33 +1,33 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.conf import settings
 # Create your models here.
 
 # 배우
 class Actor(models.Model):
-    pk = models.PositiveIntegerField()
+    id = models.PositiveIntegerField()
     name = models.CharField(max_length=100)
     profile_path = models.TextField()
 
 # 장르
 class Genre(models.Model):
-    pk = models.PositiveIntegerField()
+    id = models.PositiveIntegerField()
     name = models.CharField(max_length=100)
 
 # 제작진
 class Crew(models.Model):
-    pk = models.PositiveIntegerField()
+    id = models.PositiveIntegerField()
     name = models.CharField(max_length=100)
     job = models.CharField(max_length=100)
     profile_path = models.TextField()
 
 # 해쉬태그
 class Hashtag(models.Model):
-    pk = models.PositiveIntegerField()
+    id = models.PositiveIntegerField()
     name = models.CharField(max_length=100)
 
 # 영화
 class Movie(models.Model):
-    pk = models.PositiveIntegerField()
+    id = models.PositiveIntegerField()
     title = models.TextField()
     overview = models.TextField()
     release_date = models.DateField()
@@ -43,18 +43,18 @@ class Movie(models.Model):
 # 리뷰
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
     rank = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    like_users = models.ManyToManyField(get_user_model(), related_name='like_reviews')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
 
 
 # 리뷰댓글
 class Comment(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
