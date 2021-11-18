50f5b3d1bdf7c5b6370f71d39d16be6057ba610e
sungayoung <chengeying1025@gmail.com>
2021-11-18 16:32:28 +0900

    [ADD] 주요 통신 함수  vuex에 선언

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -9,6 +9,148 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    getReview: function ({ commit }, movie_pk) {
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/${movie_pk}/reviews/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'get',
+        })
+        .then( res => {
+          resolve(res.data)
+        })
+        .catch( err => {
+          reject(err.data)
+        })
+      })
+    },
+    createReview: function ({ commit }, movie_pk, params) {
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/${movie_pk}/reviews/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'get',
+          params,
+        })
+        .then( res => {
+          resolve(res.data)
+        })
+        .catch( err => {
+          reject(err.data)
+        })
+      })
+    },
+    updateReview: function ({ commit }, review_pk, params) {
+      
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/reviews/${review_pk}/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'put',
+          params,
+        })
+        .then( res => {
+          resolve(res.data)
+        })
+        .catch( err => {
+          reject(err.data)
+        })
+      })
+    },
+    deleteReview: function ({ commit }, review_pk) {
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/reviews/${review_pk}/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'delete',
+        })
+        .then( res => {
+          resolve(res.data)
+        })
+        .catch( err => {
+          reject(err.data)
+        })
+      })
+    },
+    getComment: function({ commit }, review_pk) {
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/reviews/${review_pk}/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'get',
+        })
+        .then( res => {
+          resolve(res.data)
+        })
+        .catch( err => {
+          reject(err.data)
+        })
+      })
+    },
+    createComment: function({ commit }, review_pk, params) {
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/reviews/${review_pk}/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'post',
+          params,
+        })
+        .then( res => {
+          return res.data
+        })
+        .catch( err => {
+          return err.data
+        })
+      })
+    },
+    updateComment: function({ commit }, review_pk, params) {
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/reviews/${review_pk}/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'put',
+          params,
+        })
+        .then( res => {
+          resolve(res.data)
+        })
+        .catch( err => {
+          reject(err.data)
+        })
+      })
+    },
+    createComment: function({ commit }, review_pk, params) {
+      commit
+      const token = localStorage.getItem('jwt')
+      return new Promise (( resolve, reject ) => {
+        axios({
+          url: `${process.env.VUE_APP_MCS_URL}/movies/reviews/${review_pk}/`,
+          headers: { Authorization: `JWT ${token}` },
+          method: 'post',
+          params,
+        })
+        .then( res => {
+          resolve(res.data)
+        })
+        .catch( err => {
+          reject(err.data)
+        })
+      })
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import Search from '@/views/movies/Search.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Search,
   }
 }
 </script>
