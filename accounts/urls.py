790c994df74edcab0fe97a9edb1a170e0458d4eb
okdongdong <kjch6411@naver.com>
2021-11-19 21:46:55 +0900

    [EDIT] user url 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,10 +7,6 @@ app_name='accounts'
 urlpatterns = [
     path('signup/', views.signup),
     path('api-token-auth/', obtain_jwt_token),
-<<<<<<< HEAD
-    # path('chatting/', views.chatting),
-=======
     path('chatting/', views.chatting),
     path('set-profile-image/', views.set_profile_image)
->>>>>>> 053e7dfb18e421ec97a9a7644ced8b1a2b799c5f
 ]
